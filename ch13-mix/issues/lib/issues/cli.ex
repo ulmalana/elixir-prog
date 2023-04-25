1225a290e0cfa01dda80ecf0a79f6017dd0296af
rz <muhamaul@gmail.com>
2023-04-25 23:52:55 +0700

    finished chapter 13

--- ch13-mix/issues/lib/issues/cli.ex
+++ ch13-mix/issues/lib/issues/cli.ex
@@ -1,11 +1,13 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @default_count 4
 
   @moduledoc """
   Handle the CLI parsing and dispatch to various functions.
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -39,7 +41,29 @@ defmodule Issues.CLI do
     """
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def decode_response({:ok, body}), do: body 
+
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
   end
 end
--- ch13-mix/issues/mix.exs
+++ ch13-mix/issues/mix.exs
@@ -4,9 +4,12 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      name: "Github Issues Fetcher",
+      source_url: "https://github.com/ulmalana/elixir-prog",
       deps: deps()
     ]
   end
@@ -23,7 +26,16 @@ defmodule Issues.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:httpoison, "~> 2.1.0"}
+      {:httpoison, "~> 2.1.0"},
+      {:poison, "~> 5.0"},
+      {:ex_doc, "~> 0.19"},
+      {:earmark, "~> 1.4.37"}
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
